901445ef838ce7108688917fd4e2e9af3b8bcd6a
MitchellWoodUk <woo15904504@btc.ac.uk>
2023-04-12 09:25:52 +0100

    Initial Commit

---
+++ AssignmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentApp
+//
+//  Created by Mitchell Wood (Student) on 12/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
