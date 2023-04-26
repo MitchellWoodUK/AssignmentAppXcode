edda1620f910c0cff08d8ef800bc2c32f81d7c41
MitchellWoodUk <mitchell_wood@btinternet.com>
2023-04-26 16:02:49 +0100

    trying to fix

--- AssignmentApp/NewProjectViewController.swift
+++ AssignmentApp/NewProjectViewController.swift
@@ -1,29 +1,51 @@
-//
-//  NewProjectViewController.swift
-//  AssignmentApp
-//
-//  Created by Mitchell Wood (Student) on 19/04/2023.
-//
+
 
 import UIKit
 
 class NewProjectViewController: UIViewController {
 
+
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var descriptionField: UITextField!
+    
+    @IBOutlet var startDateField: UIDatePicker!
+    
+    @IBOutlet var endDateField: UIDatePicker!
+    
+    @IBOutlet var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        guard let name = nameField.text, !name.isEmpty,
+              let description = descriptionField.text, !description.isEmpty,
+              let start_date = startDateField?.date.description,
+              let end_date = endDateField?.date.description
+               else {
+            return
+        }
+        
+        
+        let object = ["user_id" : UserData.shared.currentUser?.id ?? "", "name" : name, "description": description, "start_date": start_date, "end_date": end_date] as [String : Any]
+        
+        
+     
+         
+    
+        //Create the url object with the API endpoint.
+        let url = "http://127.0.0.1:5000/api/projects/add"
+        //Pass in the URL that will be executed when the fetch completes. Outputs a result if successful.
+        URLSession.shared.postData(object, urlString: url){ (result: Result<[Project], Error>) in
+            switch result {
+            case .success(let results):
+                //appends the results to the array.
+                print(results)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-    */
-
 }
